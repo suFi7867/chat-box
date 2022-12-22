685f232cf4e4d481714f321ff0e2b154cf753c86
suFi7867 <noorani786.ss@gmail.com>
2022-12-22 16:34:47 +0530

    DEPLOY

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:8080";
+export const host = "https://stackbox.onrender.com";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
